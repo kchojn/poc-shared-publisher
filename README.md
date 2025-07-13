7c20086b404fce3f7df57e19d1c6566f2e87ab1e
Karol Chojnowski <karolchojnowski95@gmail.com>
2025-07-13 15:06:50 +0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# POC Shared Publisher - Phase 1
+
+Simple transaction relay between rollup sequencers.
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,17 @@
 package logger
+
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+)
+
+func New() zerolog.Logger {
+	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
+
+	if os.Getenv("LOG_FORMAT") != "json" {
+		logger = logger.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+	}
+
+	return logger
+}
